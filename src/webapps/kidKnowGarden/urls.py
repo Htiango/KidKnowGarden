0ca5c27e1da1e84b0bbb2a0e33208a84d61d1c21
Htiango <hongty106@gmail.com>
2017-10-28 16:49:40 -0400

    update the changing of login function's name

--- src/webapps/kidKnowGarden/urls.py
+++ src/webapps/kidKnowGarden/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     url(r'^$', welcome, name='welcome-page'),
     url(r'^register', register, name='register'),
-    url(r'^login', login, name='login-page'),
+    url(r'^login', login_page, name='login-page'),
     url(r'^accounts/login', LoginView.as_view(template_name='pages/logIn.html'), name='auto-login'),
     url(r'^activate/(?P<username>[^/]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         activate, name='activate'),
