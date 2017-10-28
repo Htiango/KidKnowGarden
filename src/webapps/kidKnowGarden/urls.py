34d275d206e765fa02e4bb927fb0a8d37c3f50b4
Htiango <hongty106@gmail.com>
2017-10-28 16:40:33 -0400

    add new urls

--- src/webapps/kidKnowGarden/urls.py
+++ src/webapps/kidKnowGarden/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
 from kidKnowGarden.views import *
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     url(r'^$', welcome, name='welcome-page'),
-    url(r'register', register, name='register'),
+    url(r'^register', register, name='register'),
+    url(r'^login', login, name='login-page'),
+    url(r'^accounts/login', LoginView.as_view(template_name='pages/logIn.html'), name='auto-login'),
+    url(r'^activate/(?P<username>[^/]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        activate, name='activate'),
+    url(r'^profile/setting', set_profile, name='set-profile'),
+    url(r'^home', home, name='home'),
+    url(r'^avatar/(?P<username>\w+)$', get_avatar, name='get-avatar'),
+    url(r'^user/(?P<username>.+)/$', user_page_view, name='user-link'),
+    url(r'^logout', logout_view, name='logout'),
 ]
