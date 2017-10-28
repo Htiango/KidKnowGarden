63f6dfd93f9a55262bb84d674f1524335360823a
Htiango <hongty106@gmail.com>
2017-10-28 00:37:25 -0400

    add register url

--- src/webapps/kidKnowGarden/urls.py
+++ src/webapps/kidKnowGarden/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-
+from kidKnowGarden.views import *
 
 urlpatterns = [
-        ]
+    url(r'^$', welcome, name='welcome-page'),
+    url(r'register', register, name='register'),
+]
