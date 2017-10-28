6b988dc0d6571da7439b53585c270d52a7ef8c26
Htiango <hongty106@gmail.com>
2017-10-28 16:41:04 -0400

    add register, login, set profile functions

--- src/webapps/kidKnowGarden/views.py
+++ src/webapps/kidKnowGarden/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from kidKnowGarden.forms import *
 from kidKnowGarden.models import *
 from django.contrib.auth.tokens import default_token_generator
 from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from mimetypes import guess_type
+from django.contrib.auth import authenticate, login, logout
 
 
+# response a welcome page. If already login, then direct to the home page
 def welcome(request):
+    user = request.user
+    if not user.is_anonymous:
+        return redirect(home)
     return render(request, 'pages/welcome.html', {})
 
 
+# If request method is get, then present the register page. Otherwise save the user, send activate email and direct to
+# a notification page.
 def register(request):
+    user = request.user
+    if not user.is_anonymous:
+        return redirect(home)
     context = {}
     form = RegisterForm()
     context['form'] = form
@@ -46,3 +59,83 @@ def register(request):
     context['email'] = form.cleaned_data['email']
 
     return render(request, 'pages/notification.html', context)
+
+
+# login page
+def login_page(request):
+    user = request.user
+    if not user.is_anonymous:
+        return redirect(home)
+    return render(request, 'pages/logIn.html', {})
+
+
+def activate(request, username, token):
+    user = User.objects.get(username=username)
+    if default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect(set_profile)
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+@login_required
+def set_profile(request):
+    context = {}
+    context['profile_form'] = ProfileForm()
+    if request.method == 'GET':
+        return render(request, 'pages/set_profile.html', context)
+
+    profile_form = ProfileForm(request.POST, request.FILES)
+    context['profile_form'] = profile_form
+
+    if not profile_form.is_valid():
+        return render(request, 'pages/set_profile.html', context)
+
+    new_profile = Profile(user=request.user,
+                          grade=profile_form.cleaned_data['grade']
+                          )
+    if profile_form.cleaned_data['avatar']:
+        new_profile.avatar = profile_form.cleaned_data['avatar']
+    if profile_form.cleaned_data['bio']:
+        new_profile.bio = profile_form.cleaned_data['bio']
+    new_profile.save()
+    return redirect(home)
+
+
+@login_required
+def home(request):
+    user = request.user
+    context = {'user': user}
+    return render(request, 'pages/home.html', context)
+
+
+@login_required
+def get_avatar(request, username):
+    user = User.objects.filter(username=username)
+    profile = get_object_or_404(Profile, user=user)
+
+    if not profile.avatar:
+        raise Http404
+
+    content_type = guess_type(profile.avatar.name)
+    return HttpResponse(profile.avatar, content_type=content_type)
+
+
+@login_required
+def user_page_view(request, username):
+    try:
+        user_select = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise Http404("User does not exist!")
+    user = request.user
+
+    context = {'user': user, 'user_select': user_select}
+    return render(request, 'pages/profile.html', context)
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect(welcome)
