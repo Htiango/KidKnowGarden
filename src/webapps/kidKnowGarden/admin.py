695ccfdaac617aa9b276775dbab6068e9aada29e
Fan Wu <wf94@live.com>
2017-10-31 00:54:57 -0400

    Add 'chat-room' model for realtime chat platform

--- src/webapps/kidKnowGarden/admin.py
+++ src/webapps/kidKnowGarden/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from kidKnowGarden.models import Rooms
+
+admin.site.register(
+    Rooms,
+    list_display=["id", "title", "staff_only"],
+    list_display_links=["id", "title"],
+)
--- src/webapps/kidKnowGarden/models.py
+++ src/webapps/kidKnowGarden/models.py
@@ -2,10 +2,45 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+from channels import Group
+import json
+
 
 class LoggedInUser(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='logged_in_user')
 
+
+class Rooms(models.Model):
+    """
+    A room for people to exchange information.
+    """
+    # Room title
+    title = models.CharField(max_length=255)
+
+    # If only "staff" users are allowed (is_staff on django's User)
+    staff_only = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def websocket_group(self):
+        """
+        Returns the Channels Group that sockets should subscribe to to get sent
+        messages as they are generated.
+        """
+        return Group("room-%s" % self.id)
+
+    def send_message(self, message, user, random_room):
+        """
+        Called to send a message to the room on behalf of a user.
+        """
+        final_msg = {'room': str(self.id), 'message': message, 'username': user.username, 'random_room': random_room}
+
+        # Send out the message to everyone in the room
+        self.websocket_group.send(
+            {"text": json.dumps(final_msg)}
+        )
 
 # User's profile
 class Profile(models.Model):
