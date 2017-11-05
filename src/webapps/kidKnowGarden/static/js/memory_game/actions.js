9dc366b3581c09f707cdc3aaa7d7aaafdae35aa6
Htiango <hongty106@gmail.com>
2017-11-05 18:03:41 -0500

    change the js of memory game

--- src/webapps/kidKnowGarden/static/js/memory_game/actions.js
+++ src/webapps/kidKnowGarden/static/js/memory_game/actions.js
@@ -1,4 +1,4 @@
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F'];
 var memory_values = [];
 var memory_tile_ids = [];
 var tiles_flipped = 0;
@@ -67,9 +67,9 @@ function flipCardBack() {
   // Flip the 2 tiles back over
   var tile_1 = document.getElementById(memory_tile_ids[0]);
   var tile_2 = document.getElementById(memory_tile_ids[1]);
-  tile_1.style.background = '#FF3399';
+  tile_1.style.background = '#9fcdff';
   tile_1.innerHTML = "";
-  tile_2.style.background = '#FF3399';
+  tile_2.style.background = '#9fcdff';
   tile_2.innerHTML = "";
 
   // Clear both arrays
