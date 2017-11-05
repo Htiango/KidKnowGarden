a28b0bdc58b5e2c8ada8d7b1586edb8042b77674
Htiango <hongty106@gmail.com>
2017-11-05 12:00:33 -0500

    add question using admin

--- src/webapps/kidKnowGarden/admin.py
+++ src/webapps/kidKnowGarden/admin.py
@@ -1,9 +1,10 @@
 # Register your models here.
 from django.contrib import admin
-from kidKnowGarden.models import Rooms
+from kidKnowGarden.models import *
 
 admin.site.register(
     Rooms,
     list_display=["id", "title", "staff_only"],
     list_display_links=["id", "title"],
 )
+admin.site.register(Question)
